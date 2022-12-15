5ba611857386cdc8648f91b447351342f21a13e9
shohan-pherones <shohan.sub.56@gmail.com>
2022-12-16 01:07:02 +0600

    crud added

--- index.js
+++ index.js
@@ -38,6 +38,21 @@ async function run() {
       const service = await serviceCollection.findOne(query);
       res.send(service);
     });
+
+    // POST
+    app.post("/services", async (req, res) => {
+      const newService = req.body;
+      const result = await serviceCollection.insertOne(newService);
+      res.send(result);
+    });
+
+    // DELETE
+    app.delete("/services/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: ObjectId(id) };
+      const result = await serviceCollection.deleteOne(query);
+      res.send(result);
+    });
   } finally {
   }
 }
