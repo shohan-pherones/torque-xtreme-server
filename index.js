87aeac6f9bb4ff79fb513fb6c684708fc9543e54
shohan-pherones <shohan.sub.56@gmail.com>
2022-12-16 04:38:50 +0600

    jwt added

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require("express");
 const cors = require("cors");
 require("dotenv").config();
 const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
+const jwt = require("jsonwebtoken");
 
 const app = express();
 const port = process.env.PORT || 5000;
@@ -10,7 +11,7 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
-//moongodb
+// moongodb
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.heknll9.mongodb.net/?retryWrites=true&w=majority`;
 
 const client = new MongoClient(uri, {
@@ -24,7 +25,16 @@ async function run() {
     const serviceCollection = client.db("torqueXtreme").collection("services");
     const orderCollection = client.db("torqueXtreme").collection("orders");
 
-    // GET API
+    // AUTH
+    app.post("/login", async (req, res) => {
+      const user = req.body;
+      const accessToken = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, {
+        expiresIn: "1d",
+      });
+      res.send({ accessToken });
+    });
+
+    // SERVICES API
     app.get("/services", async (req, res) => {
       const query = {};
       const cursor = serviceCollection.find(query);
@@ -32,7 +42,6 @@ async function run() {
       res.send(services);
     });
 
-    // GET ONE API
     app.get("/services/:id", async (req, res) => {
       const id = req.params.id;
       const query = { _id: ObjectId(id) };
@@ -40,14 +49,12 @@ async function run() {
       res.send(service);
     });
 
-    // POST
     app.post("/services", async (req, res) => {
       const newService = req.body;
       const result = await serviceCollection.insertOne(newService);
       res.send(result);
     });
 
-    // DELETE
     app.delete("/services/:id", async (req, res) => {
       const id = req.params.id;
       const query = { _id: ObjectId(id) };
@@ -55,7 +62,7 @@ async function run() {
       res.send(result);
     });
 
-    // Order collection API
+    // ORDERS API
     app.get("/orders", async (req, res) => {
       const email = req.query.email;
       const query = { email };
